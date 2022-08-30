1b3ad9427a101e304bb277a849a8128dcfe3baf5
Sachin_Pandey <sachinpandey262523@gmail.com>
2022-08-31 00:22:54 +0530

    searchUser

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express=require("express")
  const router=express.Router()
- const {registerUser,authUser}=require("../controller/userController")
+ const {registerUser,authUser,allUsers}=require("../controller/userController")
 
 //  registration
-router.route('/').post(registerUser)
+router.route('/').post(registerUser);
 // authentication
  router.post("/login",authUser)
 //  searching
